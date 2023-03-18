6078191954691dd63cfe89f3ae693cbadb9a71b6
Aritra <103651291+Aritra212@users.noreply.github.com>
2023-03-18 17:30:51 +0530

    Circular Queue

--- cqueue.c
+++ cqueue.c
@@ -1,49 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 5
+#define MAX 3
 
 int queue[MAX], front= -1, rear= -1, item;
 
 void insert(){
-    if(front==(rear+1)%MAX){
+    if(front==(rear+1)%MAX)
         printf("\nQueue is full");
-    }
     else{
         printf("\nEnter item:: ");
         scanf("%d",&item);
-        if(front==-1 && rear== -1){
+        if(front==-1 && rear== -1)
             front= rear= 0;
-        }
-        else{
+        else
             rear= (rear+1)%MAX;
-        }
         queue[rear]=item;
         printf("\n%d inserted.",queue[rear]);
     }
 }
 void dequeue(){
-    if(front==-1){
+    if(front==-1)
         printf("\nQueue is empty");
-    }
     else{
         item= queue[front];
         queue[front]=NULL;
         if(front == rear){
         	front=-1;
-			rear=-1;
-		}
-		else{
+		rear=-1;
+	   }
+	   else
         	front= (front+1)%MAX;
-		}
         printf("%d deleted.",item);
     }
 
 }
 void display(){
     int i=front,c;
-    if(front== -1 && rear== -1){
+    if(front== -1 && rear== -1)
         printf("\nQueue is empty");
-    }
     else{
         printf("\nQueue is : ");
         while(i!=rear){
@@ -69,9 +63,9 @@ int main(){
     int n;
     while(1){
         printf("\n-------------------------------------\n");
-        printf("\nEnter '1' to insert elsement.");
-        printf("\nEnter '2' to delete elsement.");
-        printf("\nEnter '3' to display elsement.");
+        printf("\nEnter '1' to insert element.");
+        printf("\nEnter '2' to delete element.");
+        printf("\nEnter '3' to display element.");
         printf("\nEnter '0' to  exit.");
         printf("\nEnter your choice:: ");
         scanf("%d",&n);
