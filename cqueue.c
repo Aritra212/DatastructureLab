0db32703ccd509ee30598c2a887ac977cf0377d8
Aritra <103651291+Aritra212@users.noreply.github.com>
2023-03-18 17:19:26 +0530

    Circular Queue

---
+++ cqueue.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 2
+
+int queue[MAX], front= -1, rear= -1, item;
+
+void insert(){
+    if(front==(rear+1)%MAX){
+        printf("\nQueue is full");
+    }
+    else{
+        printf("\nEnter item:: ");
+        scanf("%d",&item);
+        if(front==-1 && rear== -1){
+            front= rear= 0;
+        }
+        else{
+            rear= (rear+1)%MAX;
+        }
+        queue[rear]=item;
+        printf("\n%d inserted.",queue[rear]);
+    }
+}
+void dequeue(){
+    if(front==-1){
+        printf("\nQueue is empty");
+    }
+    else{
+        item= queue[front];
+        queue[front]=NULL;
+        if(front == rear){
+        	front=-1;
+			rear=-1;
+		}
+		else{
+        	front= (front+1)%MAX;
+		}
+        printf("%d deleted.",item);
+    }
+
+}
+void display(){
+    int i=front,c;
+    if(front== -1 && rear== -1){
+        printf("\nQueue is empty");
+    }
+    else{
+        printf("\nQueue is : ");
+        while(i!=rear){
+            if(queue[i]==NULL){
+                c=0;
+            }
+            else{
+                printf("%d ",queue[i]);
+                i= (i+1)%MAX;
+                c++;
+            }
+        }
+        if(c==0){
+            printf("\nQueue is empty");
+        }
+        else{
+            printf("%d",queue[rear]);
+        }
+    }
+}
+
+int main(){
+    int n;
+    while(1){
+        printf("\n-------------------------------------\n");
+        printf("\nEnter '1' to insert elsement.");
+        printf("\nEnter '2' to delete elsement.");
+        printf("\nEnter '3' to display elsement.");
+        printf("\nEnter '0' to  exit.");
+        printf("\nEnter your choice:: ");
+        scanf("%d",&n);
+
+        switch(n){
+            case 1: insert();
+            break;
+            case 2: dequeue();
+            break;
+            case 3: display();
+            break;
+            case 0: exit(0);
+            default: printf("\nInvalid choice");
+        }
+    }
+}
